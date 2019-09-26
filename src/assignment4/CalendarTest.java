4b09555cdac9bbce4a84aab7b8ec79b46a08fe5b
fatemehnouri <fatemeh.nouri@ridgebluedynamics.com>
2019-09-26 10:21:48 -0400

    final.

--- src/assignment4/CalendarTest.java
+++ src/assignment4/CalendarTest.java
@@ -10,7 +10,7 @@ import java.util.*;
 public class CalendarTest
 {
 
-    private final InputStream systemIn = System.in;
+    private final InputStream inputStream = System.in;
     public static void main(String args[]){
         org.junit.runner.JUnitCore.main("CalendarTest");
     }
@@ -22,19 +22,9 @@ public class CalendarTest
         output = new ByteArrayOutputStream();
         System.setOut(new PrintStream(output));
     }
-
-    private void provideCommandLineArguments(String inputData) {
-        input = new ByteArrayInputStream(inputData.getBytes());
-        System.setIn(input);
-    }
-
-    private String getCalendarResult() {
-        return output.toString();
-    }
-
     @After
     public void restoreSystemInputOutput() {
-        System.setIn(systemIn);
+        System.setIn(inputStream);
         System.setOut(outputStream);
     }
 
@@ -44,14 +34,15 @@ public class CalendarTest
     @Test
     public void HappyPathTest() {
         final String inputString = "9\n2019";
-        provideCommandLineArguments(inputString);
+        input = new ByteArrayInputStream(inputString.getBytes());
+        System.setIn(input);
         String expectedResult = "Enter month: Enter year:    September 2019\n"+" S  M Tu  W Th  F  S\n"+" 1  2  3  4  5  6  7 \n"+
                 " 8  9 10 11 12 13 14 \n"+"15 16 17 18 19 20 21 \n"+"22 23 24 25 26 27 28 \n"+
                 "29 30 \n";
 
         Calendar.main(new String[0]);
-
-        assertTrue("Can't pass the Happy Path test: possible mistakes in the design of the algorithm.\n",expectedResult.equals(getCalendarResult()));
+        String actualResult = output.toString();
+        assertTrue("Can't pass the Happy Path test: possible mistakes in the design of the algorithm.\n",expectedResult.equals(actualResult));
     }
     /**
      * This test case checks for a leap year: testing if a leap year is identified.
@@ -59,11 +50,13 @@ public class CalendarTest
     @Test
     public void leapYearTest1() {
         final String inputString = "2\n2020";
-        provideCommandLineArguments(inputString);
+        input = new ByteArrayInputStream(inputString.getBytes());
+        System.setIn(input);
         String expectedResult = "Enter month: Enter year:    February 2020\n"+" S  M Tu  W Th  F  S\n"+"                   1 \n"+" 2  3  4  5  6  7  8 \n"+
                 " 9  10 11 12 13 14 15 \n"+"16 17 18 19 20 21 22 \n"+"23 24 25 26 27 28 29 \n";
         Calendar.main(new String[0]);
-        assertTrue("The leap year calculation is incorrect.\n",expectedResult.equals(getCalendarResult()));
+        String actualResult = output.toString();
+        assertTrue("The leap year calculation is incorrect.\n",expectedResult.equals(actualResult));
     }
     /**
      * This test case checks for a non-leap year: testing if the leap year algorithm is correct.
@@ -71,12 +64,14 @@ public class CalendarTest
     @Test
     public void leapYearTest2() {
         final String inputString = "2\n2019";
-        provideCommandLineArguments(inputString);
+        input = new ByteArrayInputStream(inputString.getBytes());
+        System.setIn(input);
         String expectedResult = "Enter month: Enter year:    February 2019\n"+" S  M Tu  W Th  F  S\n"+"                1  2 \n"+" 3  4  5  6  7  8  9 \n"+
                 "10 11 12 13 14 15 16 \n"+"17 18 19 20 21 21 23 \n"+
                 "24 25 26 27 28 \n";
         Calendar.main(new String[0]);
-        assertTrue("The leap year algorithm is incorrect.\n",expectedResult.equals(getCalendarResult()));
+        String actualResult = output.toString();
+        assertTrue("The leap year algorithm is incorrect.\n",expectedResult.equals(actualResult));
     }
     /**
      * This test case checks for a invalid inputs: testing if an invalid month such as 0 is handled correctly.
@@ -84,10 +79,12 @@ public class CalendarTest
     @Test
     public void ZeroMonthTest() {
         final String inputString = "0\n2019";
-        provideCommandLineArguments(inputString);
+        input = new ByteArrayInputStream(inputString.getBytes());
+        System.setIn(input);
         String expectedResult = "Invalid Input: The value for month may not be 0.";
         Calendar.main(new String[0]);
-        assertTrue("The exception paths are not tested thoroughly: month=0\n",expectedResult.equals(getCalendarResult()));
+        String actualResult = output.toString();
+        assertTrue("The exception paths are not tested thoroughly: month=0\n",expectedResult.equals(actualResult));
     }
     /**
      * This test case checks for a invalid inputs: testing if an invalid year such as a negative year is handled correctly.
@@ -95,10 +92,12 @@ public class CalendarTest
     @Test
     public void NegativeYearTest() {
         final String inputString = "5\n-3";
-        provideCommandLineArguments(inputString);
+        input = new ByteArrayInputStream(inputString.getBytes());
+        System.setIn(input);
         String expectedResult = "Invalid Input: The value for year may not be a negative value.";
         Calendar.main(new String[0]);
-        assertTrue("The exception paths are not tested thoroughly: year<0\n",expectedResult.equals(getCalendarResult()));
+        String actualResult = output.toString();
+        assertTrue("The exception paths are not tested thoroughly: year<0\n",expectedResult.equals(actualResult));
     }
     /**
      * This test case checks for a invalid inputs: testing if an invalid month such as a negative month is handled correctly.
@@ -106,10 +105,12 @@ public class CalendarTest
     @Test
     public void NegativeMonthTest() {
         final String inputString = "-5\n2019";
-        provideCommandLineArguments(inputString);
+        input = new ByteArrayInputStream(inputString.getBytes());
+        System.setIn(input);
         String expectedResult = "Invalid Input: The value for month may not be a negative value.";
         Calendar.main(new String[0]);
-        assertTrue("The exception paths are not tested thoroughly: month<0\n",expectedResult.equals(getCalendarResult()));
+        String actualResult = output.toString();
+        assertTrue("The exception paths are not tested thoroughly: month<0\n",expectedResult.equals(actualResult));
     }
     /**
      * This test case checks for a invalid inputs: testing if an invalid input type such as a string is handled correctly.
@@ -117,9 +118,11 @@ public class CalendarTest
     @Test
     public void StringInputTest() {
         final String inputString = "January\n2019";
-        provideCommandLineArguments(inputString);
+        input = new ByteArrayInputStream(inputString.getBytes());
+        System.setIn(input);
         String expectedResult = "Invalid Input: The value for month may only be of type int.";
         Calendar.main(new String[0]);
-        assertTrue("The exception paths are not tested thoroughly: Unexpected Types\n",expectedResult.equals(getCalendarResult()));
+        String actualResult = output.toString();
+        assertTrue("The exception paths are not tested thoroughly: Unexpected Types\n",expectedResult.equals(actualResult));
     }
 }
